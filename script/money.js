2590877cf804e69243806acfc155ecc2b48b56b8
Iftekher Aziz <azizchy02@gmail.com>
2023-02-16 21:29:56 +0600

    commit

--- script/money.js
+++ script/money.js
@@ -19,33 +19,6 @@ Event handler:
 
 */
 
-/*  This function is to get all input field */
-
-function getInputValue(id) {
-  const inputField = document.getElementById(id);
-  const inputFieldString = inputField.value;
-  const inputFieldMoney = parseFloat(inputFieldString);
-  return inputFieldMoney;
-}
-
-/* This function is to set input field value into the innerText field */
-
-function setInnerTextValue(id, value) {
-  const outputField = document.getElementById(id);
-  outputField.innerText = value.toFixed(2);
-}
-
-/* This fucntion is to fix Error handling*/
-
-function validation(errors) {
-  for (let error of errors) {
-    if (isNaN(error) || error < 0 || error == "") {
-      return false;
-    }
-  }
-  return true;
-}
-
 /* Now set event handler to the calculate-button*/
 
 document
