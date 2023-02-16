1cb30dba2da33ba31873f59de09d2027a8331590
Iftekher Aziz <azizchy02@gmail.com>
2023-02-16 21:19:06 +0600

    Commit

--- script/money.js
+++ script/money.js
@@ -1,23 +1,96 @@
+/* 
+
+Input Field: 
+1. income
+2. food
+3. rent
+4. clothes
+5. save
+
+Output Field:
+1. expense
+2. balance
+3. save-amount
+4. remaining-amount
+
+Event handler: 
+1. calculate-button
+2. save-button
+
+*/
+
+/*  This function is to get all input field */
+
+function getInputValue(id) {
+  const inputField = document.getElementById(id);
+  const inputFieldString = inputField.value;
+  const inputFieldMoney = parseFloat(inputFieldString);
+  return inputFieldMoney;
+}
+
+/* This function is to set input field value into the innerText field */
+
+function setInnerTextValue(id, value) {
+  const outputField = document.getElementById(id);
+  outputField.innerText = value.toFixed(2);
+}
+
+/* This fucntion is to fix Error handling*/
+
+function validation(errors) {
+  for (let error of errors) {
+    if (isNaN(error) || error < 0 || error == "") {
+      return false;
+    }
+  }
+  return true;
+}
+
+/* Now set event handler to the calculate-button*/
+
 document
   .getElementById("calculate-button")
   .addEventListener("click", function () {
-    const income = document.getElementById("income").value;
-    const food = document.getElementById("food").value;
-    const rent = document.getElementById("rent").value;
-    const clothes = document.getElementById("clothes").value;
+    const income = getInputValue("income");
+    const food = getInputValue("food");
+    const rent = getInputValue("rent");
+    const clothes = getInputValue("clothes");
+    const expense = food + rent + clothes;
+    const validInput = validation([income, food, rent, clothes]);
 
-    // calculate expense
-    const expense = parseInt(food) + parseInt(rent) + parseInt(clothes); 
-    
-    // calculate balance
-    const balance = parseInt(income.value) - expense; 
-    
-    //   validate income
-    if (expense > income.value) {
-      alert("Expenses cannot be more than income");
+    if (validInput === false) {
+      alert("Please input numeric positive balance");
     } else {
-      // view total expense and balance
-      document.getElementById("expense").innerText = expense;
-      document.getElementById("balance").innerText = balance;
+      if (expense > income) {
+        alert("You do not have enough money");
+      } else {
+        setInnerTextValue("expense", expense);
+        const balance = income - expense;
+        setInnerTextValue("balance", balance);
+      }
     }
   });
+
+/* Now set event handler to the save-button*/
+
+document.getElementById("save-button").addEventListener("click", function () {
+  const income = getInputValue("income");
+  const save = getInputValue("save");
+  const previousBanlance = document.getElementById("balance").innerText;
+
+  const validInput = validation([income, save, previousBanlance]);
+
+  if (validInput === false) {
+    alert("Please input numeric positive balance");
+  } else {
+    const saveAmount = (income * save) / 100;
+    if (previousBanlance - saveAmount < 0) {
+      alert("You do not have enough balance");
+    }
+    else {
+    setInnerTextValue("save-amount", saveAmount);
+    const remainingAmount = previousBanlance - saveAmount;
+    setInnerTextValue("remaining-amount", remainingAmount);
+  }
+}
+});
